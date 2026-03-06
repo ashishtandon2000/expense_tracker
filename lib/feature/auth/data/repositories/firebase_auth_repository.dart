20f4f810df81294351a49631287577f36fd88879
ashishtandon2000 <tandonashish15@gmail.com>
2026-03-06 22:31:53 +0530

    add data layer: auth service & auth repository

--- lib/feature/auth/data/repositories/firebase_auth_repository.dart
+++ lib/feature/auth/data/repositories/firebase_auth_repository.dart
@@ -24,3 +24,42 @@ This is where:
     Domain layer
     It ensures clean separation and testability.
 */
+
+
+ import 'package:firebase_auth/firebase_auth.dart';
+
+ import '../../domain/entities/app_user.dart';
+ import '../../domain/repositories/auth_repository.dart';
+ import '../services/firebase_auth_service.dart';
+
+ class FirebaseAuthRepository implements AuthRepository {
+   final FirebaseAuthService _authService;
+
+   FirebaseAuthRepository(this._authService);
+
+   @override
+   Stream<AppUser?> authStateChanges() {
+     return _authService.authStateChanges().map(_mapUser);
+   }
+
+   @override
+   Future<void> signInWithGoogle() async {
+     await _authService.signInWithGoogle();
+   }
+
+   @override
+   Future<void> signOut() async {
+     await _authService.signOut();
+   }
+
+   AppUser? _mapUser(User? firebaseUser) {
+     if (firebaseUser == null) return null;
+
+     return AppUser(
+       id: firebaseUser.uid,
+       email: firebaseUser.email,
+       displayName: firebaseUser.displayName,
+       photoUrl: firebaseUser.photoURL,
+     );
+   }
+ }
--- lib/feature/auth/data/services/firebase_auth_service.dart
+++ lib/feature/auth/data/services/firebase_auth_service.dart
@@ -18,3 +18,45 @@
 🧠 Why It Exists
     Separates raw SDK interaction from repository logic.
 */
+
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FirebaseAuthService{
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  FirebaseAuthService({
+    FirebaseAuth? firebaseAuth,
+    GoogleSignIn? googleSignIn,
+  })  : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+        _googleSignIn = googleSignIn ?? GoogleSignIn.instance;
+
+  Future<void> initialize() async {
+    await _googleSignIn.initialize();
+  }
+
+  Stream<User?> authStateChanges() {
+    return _firebaseAuth.authStateChanges();
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+
+    final account = await _googleSignIn.authenticate();
+
+    final googleAuth = account.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.idToken,
+      idToken: googleAuth.idToken,
+    );
+
+    return await _firebaseAuth.signInWithCredential(credential);
+  }
+
+  Future<void> signOut() async {
+    await _googleSignIn.signOut();
+    await _firebaseAuth.signOut();
+  }
+}
